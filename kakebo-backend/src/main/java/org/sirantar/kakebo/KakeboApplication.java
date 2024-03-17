52f460a9fdec3643882e94d273a7ce16351bdd82
srn <salri@ATUIN-WIN>
2024-03-17 17:50:04 +0100

    feat(backend): add backend first commit

---
+++ kakebo-backend/src/main/java/org/sirantar/kakebo/KakeboApplication.java
@@ -0,0 +1,13 @@
+package org.sirantar.kakebo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakeboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakeboApplication.class, args);
+	}
+
+}
---
+++ kakebo-backend/src/test/java/org/sirantar/kakebo/KakeboApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sirantar.kakebo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakeboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
