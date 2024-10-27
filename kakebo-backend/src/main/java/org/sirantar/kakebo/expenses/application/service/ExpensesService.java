cf87fb6779dc3134cfa2e1b39e5a42986753c93a
srn <salrio74@gmail.com>
2024-10-27 23:16:49 +0100

    fix: some code fixes

--- kakebo-backend/src/main/java/org/sirantar/kakebo/expenses/application/service/ExpensesService.java
+++ kakebo-backend/src/main/java/org/sirantar/kakebo/expenses/application/service/ExpensesService.java
@@ -3,7 +3,13 @@ package org.sirantar.kakebo.expenses.application.service;
 import org.sirantar.kakebo.expenses.application.dto.ExpenseDTO;
 import org.sirantar.kakebo.expenses.domain.model.Expenses;
 import org.sirantar.kakebo.expenses.domain.repository.ExpensesRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ExpensesService {
 
 	private final ExpensesRepository expensesRepository;
@@ -13,10 +19,18 @@ public class ExpensesService {
 	}
 
 
-	// TODO - here getExpensed, save ect
+	public List<ExpenseDTO> getExpenses(){
+
+		List<ExpenseDTO> result = new ArrayList<>();
+
+		// TODO - get from respository
+		result.add(new ExpenseDTO(1));
+
+		return result;
+	}
 
 	public ExpenseDTO getExpenseById(Long id){
-		Expenses expenses = expensesRepository.findById(id);
-		return new ExpenseDTO(expenses.getId());
+		Optional<Expenses> expenses = expensesRepository.findById(id);
+		return expenses.map(value -> new ExpenseDTO(value.getId())).orElseGet(() -> new ExpenseDTO(1));
 	}
 }
--- kakebo-backend/src/main/java/org/sirantar/kakebo/expenses/domain/repository/ExpensesRepository.java
+++ kakebo-backend/src/main/java/org/sirantar/kakebo/expenses/domain/repository/ExpensesRepository.java
@@ -4,12 +4,13 @@ package org.sirantar.kakebo.expenses.domain.repository;
 import org.sirantar.kakebo.expenses.domain.model.Expenses;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ExpensesRepository {
 
 	Expenses save(Expenses expense);
 
-	Expenses findById(Long id);
+	Optional<Expenses> findById(Long id);
 
 	List<Expenses> findAll();
 
