3d4d67125d670e6c8dd0790707aab574ac9d4b39
srn <salrio74@gmail.com>
2024-11-28 00:27:00 +0100

    feat(income): added base logic to access data and rest response

--- kakebo-backend/src/main/java/org/sirantar/kakebo/income/infrastructure/rest/IncomeController.java
+++ kakebo-backend/src/main/java/org/sirantar/kakebo/income/infrastructure/rest/IncomeController.java
@@ -1,15 +1,33 @@
 package org.sirantar.kakebo.income.infrastructure.rest;
 
+import org.sirantar.kakebo.income.applictation.service.IncomeService;
+import org.sirantar.kakebo.income.domain.model.Income;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/incomes/")
+@RequestMapping("/")
 public class IncomeController {
 
-    @GetMapping("/income/")
-    public void getIncome(){
+	private final IncomeService incomeService;
 
+	@Autowired
+	public IncomeController(IncomeService incomeService) {
+		this.incomeService = incomeService;
+	}
+
+    @GetMapping("/incomes")
+    public List<Income> getIncomes(){
+		return incomeService.getIncomes();
     }
+
+	@GetMapping("/incomes/{id}")
+	public Income getIncome(@PathVariable Long id){
+		return incomeService.getIncome(id);
+	}
 }
